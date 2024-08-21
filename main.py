4e3e559b487722ebebc3f9b1b1cb82e500a1442b
MohibShaikh <mohibuddin9@gmail.com>
2024-08-21 13:35:58 +0500

    updated dataloaders

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ def main():
     target_train_loader = mnistm.mnistm_train_loader
 
     if torch.cuda.is_available():
-        encoder = model.Extractor().cuda()
-        classifier = model.Classifier().cuda()
-        discriminator = model.Discriminator().cuda()
+        encoder = model.Extractor3D().cuda()
+        classifier = model.Classifier3D().cuda()
+        discriminator = model.Discriminator3D().cuda()
 
         train.source_only(encoder, classifier, source_train_loader, target_train_loader)
         train.dann(encoder, classifier, discriminator, source_train_loader, target_train_loader)
--- mnist.py
+++ mnist.py
@@ -1,55 +1,120 @@
 import torchvision.datasets as datasets
 from torch.utils.data import SubsetRandomSampler, DataLoader
 from torchvision import transforms
+import torch.utils.data as data
 import torch
-import params
-
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.1307,), (0.3081,))
-                                ])
-
-mnist_train_dataset = datasets.MNIST(root='../data/MNIST', train=True, download=True,
-                                     transform=transform)
-mnist_valid_dataset = datasets.MNIST(root='../data/MNIST', train=True, download=True,
-                                     transform=transform)
-mnist_test_dataset = datasets.MNIST(root='../data/MNIST', train=False, transform=transform)
-
-indices = list(range(len(mnist_train_dataset)))
-validation_size = 5000
-train_idx, valid_idx = indices[validation_size:], indices[:validation_size]
-train_sampler = SubsetRandomSampler(train_idx)
-valid_sampler = SubsetRandomSampler(valid_idx)
-
-mnist_train_loader = DataLoader(
-    mnist_train_dataset,
-    batch_size=params.batch_size,
-    sampler=train_sampler,
-    num_workers=params.num_workers
-)
-
-mnist_valid_loader = DataLoader(
-    mnist_valid_dataset,
-    batch_size=params.batch_size,
-    sampler=train_sampler,
-    num_workers=params.num_workers
-)
-
-mnist_test_loader = DataLoader(
-    mnist_test_dataset,
-    batch_size=params.batch_size,
-    num_workers=params.num_workers
-)
+import numpy as np
+import os
+from params import *
+import trimesh
+from PIL import Image
 
 
-def one_hot_embedding(labels, num_classes=10):
-    """Embedding labels to one-hot form.
+batch_size = 8
+class TargetDomainDataset(data.Dataset):
+    def __init__(self, root, npoints=1024, classes=None, transform=None):
+        self.root = root
+        self.npoints = npoints
+        self.transform = transform
 
-    Args:
-      labels: (LongTensor) class labels, sized [N,].
-      num_classes: (int) number of classes.
+        self.categories = classes if classes else self._get_categories()
+        self.files = self._get_files()
 
-    Returns:
-      (tensor) encoded labels, sized [N, #classes].
-    """
-    y = torch.eye(num_classes)
-    return y[labels]
+    def _get_categories(self):
+        return sorted(os.listdir(self.root))
+
+    def _get_files(self):
+        files = []
+        for category in self.categories:
+            category_path = os.path.join(self.root, category)
+            for model in os.listdir(category_path):
+                if model.endswith('.obj'):
+                    files.append((category, os.path.join(category_path, model)))
+        return files
+
+    def _load_model(self, path):
+        mesh = trimesh.load(path)
+        points = mesh.sample(self.npoints)
+        return points
+
+    def __getitem__(self, index):
+        category, path = self.files[index]
+        points = self._load_model(path)
+        label = self.categories.index(category)
+
+        points = torch.tensor(points, dtype=torch.float32)
+        label = torch.tensor(label, dtype=torch.long)
+
+        if self.transform:
+            points = self.transform(points)
+
+        return points, label
+
+    def __len__(self):
+        return len(self.files)
+
+
+# Transforms
+class RandomRotate(object):
+    def __call__(self, pointcloud):
+        theta = np.random.uniform(0, 2 * np.pi)
+        rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
+                                    [np.sin(theta),  np.cos(theta), 0],
+                                    [0, 0, 1]])
+        return torch.matmul(pointcloud, torch.tensor(rotation_matrix, dtype=torch.float32))
+
+class RandomScale(object):
+    def __call__(self, pointcloud):
+        scale = np.random.uniform(0.8, 1.25)
+        return pointcloud * scale
+
+class RandomTranslate(object):
+    def __call__(self, pointcloud):
+        translation = np.random.uniform(-0.2, 0.2, size=(3,))
+        return pointcloud + torch.tensor(translation, dtype=torch.float32)
+
+class Jitter(object):
+    def __init__(self, sigma=0.01, clip=0.05):
+        self.sigma = sigma
+        self.clip = clip
+
+    def __call__(self, pointcloud):
+        jittered_data = pointcloud + torch.clamp(self.sigma * torch.randn_like(pointcloud), -self.clip, self.clip)
+        return jittered_data
+
+class Normalize(object):
+    def __call__(self, pointcloud):
+        centroid = torch.mean(pointcloud, dim=0)
+        pointcloud = pointcloud - centroid
+        furthest_distance = torch.max(torch.sqrt(torch.sum(pointcloud ** 2, dim=1)))
+        pointcloud = pointcloud / furthest_distance
+        return pointcloud
+
+class ToTensor(object):
+    def __call__(self, pointcloud):
+        return torch.tensor(pointcloud, dtype=torch.float32)
+
+
+# Transform pipeline
+transform = transforms.Compose([
+    RandomRotate(),
+    RandomScale(),
+    RandomTranslate(),
+    Jitter(),
+    Normalize(),
+    ToTensor(),
+])
+
+target_root_dir = 'target'  # Target domain
+
+
+target_dataset = TargetDomainDataset(root=target_root_dir, npoints=2048, transform=transform)
+
+# Data Loader for target domain (no labels, only points)
+target_loader = DataLoader(target_dataset, batch_size=batch_size, shuffle=True)
+
+# Example: Iterate through the target domain data loader
+for points, labels in target_loader:
+    print("Target Domain Points:", points.shape)  # Should be (batch_size, npoints, 3)
+    print("Target Domain Labels:", labels.shape)  # Should be (batch_size,)
+    break
